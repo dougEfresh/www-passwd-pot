81fc9ae306b4fd26568722ee144d722b79220b72
Doug Chimento <dchimento@outbrain.com>
2017-03-09 19:58:43 -0500

    using ssl server

--- runner.js
+++ runner.js
@@ -103,7 +103,7 @@ function connect() {
 
     console.log('Connecting');
 
-    this.socket = new WebSocket("ws://stream.passwd-pot.com/api/v1/event/stream/random");
+    this.socket = new WebSocket("wss://stream.passwd-pot.com/api/v1/event/stream/random");
 
     this.socket.onopen = function () {
         console.log('Connected');
